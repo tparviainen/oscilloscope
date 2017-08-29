5ca93d85248f907129a2bdab437fbe77d3994b08
Tomi Parviainen <tomi.parviainen@gmail.com>
2017-08-29 18:16:29 +0300

    LAN interface implementation that supports V1 interface.

---
+++ LAN/LANInterface.cs
@@ -0,0 +1,53 @@
+﻿using PluginContracts;
+using System;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.IO;
+
+namespace LAN
+{
+    public class LANInterface : IPluginV1
+    {
+        public string Name { get; } = "LAN";
+
+        public string Description { get; } = "LAN communication interface for oscilloscopes such as Rigol DS1054Z";
+
+        public IPEndPoint IPEndPoint { get; set; }
+
+        public async Task<byte[]> SendReceive(string command)
+        {
+            using (var socket = new Socket(IPEndPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp))
+            {
+                socket.Connect(IPEndPoint);
+
+                // Start with the initial size of the socket receive buffer
+                using (var ms = new MemoryStream(socket.ReceiveBufferSize))
+                {
+                    socket.Send(Encoding.ASCII.GetBytes(command + "\n"));
+
+                    var data = new byte[1024];
+
+                    int received = 0;
+
+                    do
+                    {
+                        // Receive will block if no data available
+                        received = socket.Receive(data, data.Length, 0);
+
+                        // Zero bytes received means that socket has been closed by the remote host
+                        if (received != 0)
+                        {
+                            await ms.WriteAsync(data, 0, received);
+                        }
+
+                        // Read until terminator '\n' (0x0A) found from buffer
+                    } while (received != 0 && data[received - 1] != 0x0A);
+
+                    return ms.ToArray();
+                }
+            }
+        }
+    }
+}
