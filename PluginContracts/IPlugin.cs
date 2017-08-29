058989d7dffd11aeaaa5d97c8cc6cb3570dc0193
Tomi Parviainen <tomi.parviainen@gmail.com>
2017-08-29 18:15:13 +0300

    Plugin interface (version 1) for LAN connection.

---
+++ PluginContracts/IPlugin.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PluginContracts
+{
+    /// <summary>
+    /// Common data for all versions of plugin interfaces.
+    /// </summary>
+    public interface IPlugin
+    {
+        /// <summary>
+        /// The name of the interface
+        /// </summary>
+        string Name { get; }
+
+        /// <summary>
+        /// The description of the interface
+        /// </summary>
+        string Description { get; }
+    }
+}
---
+++ PluginContracts/IPluginV1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace PluginContracts
+{
+    public interface IPluginV1 : IPlugin
+    {
+        /// <summary>
+        /// IP address and port number of the connection
+        /// </summary>
+        IPEndPoint IPEndPoint { get; set; }
+
+        /// <summary>
+        /// Sends the command to instrument and waits a response from the connected instrument
+        /// </summary>
+        /// <param name="command">Command to send</param>
+        /// <returns>Received data</returns>
+        Task<byte[]> SendReceive(string command);
+    }
+}
