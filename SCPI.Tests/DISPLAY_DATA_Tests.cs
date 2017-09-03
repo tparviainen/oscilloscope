91898aed2090484072842703fe3ff9f93f62d1ec
Tomi Parviainen <tomi.parviainen@gmail.com>
2017-09-03 16:32:48 +0300

    Test code for DISPlay:DATA and IDN -commands.

---
+++ SCPI.Tests/DISPLAY_DATA_Tests.cs
@@ -0,0 +1,78 @@
+using System;
+using System.IO;
+using Xunit;
+
+namespace SCPI.Tests
+{
+    public class DISPLAY_DATA_Tests
+    {
+        [Fact]
+        public void DescriptionExists()
+        {
+            // Arrange
+            var cmd = new DISPLAY_DATA();
+
+            // Act
+            var desc = cmd.Description;
+
+            // Assert
+            Assert.NotNull(desc);
+        }
+
+        [Fact]
+        public void CommandWithoutParameters()
+        {
+            // Arrange
+            var cmd = new DISPLAY_DATA();
+            var expected = ":DISPlay:DATA?";
+
+            // Act
+            var actual = cmd.Command();
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void CommandWithColorParameter()
+        {
+            // Arrange
+            var cmd = new DISPLAY_DATA();
+            var expected = ":DISPlay:DATA? ON";
+
+            // Act
+            var actual = cmd.Command("ON");
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void CommandWithColorInvertParameters()
+        {
+            // Arrange
+            var cmd = new DISPLAY_DATA();
+            var expected = ":DISPlay:DATA? ON, OFF";
+
+            // Act
+            var actual = cmd.Command("ON", "OFF");
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void CommandWithColorInvertFormatParameters()
+        {
+            // Arrange
+            var cmd = new DISPLAY_DATA();
+            var expected = ":DISPlay:DATA? ON, OFF, BMP24";
+
+            // Act
+            var actual = cmd.Command("ON", "OFF", "BMP24");
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ SCPI.Tests/IDN_Tests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace SCPI.Tests
+{
+    public class IDN_Tests
+    {
+        [Fact]
+        public void ValidCommand()
+        {
+            // Arrange
+            var cmd = new IDN();
+            var expected = "*IDN?";
+
+            // Act
+            var actual = cmd.Command();
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ParsingCommandSucceeds()
+        {
+            // Arrange
+            var cmd = new IDN();
+            var data = "RIGOL TECHNOLOGIES, DS1054Z, DS1ZA000000000, 00.04.04.SP3\n";
+
+            // Act
+            var result = cmd.Parse(Encoding.ASCII.GetBytes(data));
+
+            // Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void ParsingCommandFails()
+        {
+            // Arrange
+            var cmd = new IDN();
+            var data = "Not a valid ID string";
+
+            // Act
+            var result = cmd.Parse(Encoding.ASCII.GetBytes(data));
+
+            // Assert
+            Assert.False(result);
+        }
+    }
+}
