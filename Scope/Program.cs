0e1ab6aef6f122dd75b7baf8fbdd21071bbccdc0
Tomi Parviainen <tomi.parviainen@gmail.com>
2017-09-03 16:34:37 +0300

    Main program + solution updates.

---
+++ Scope/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Scope
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * scope -p <plugins> -i <interface ip:p> -c command [arguments] -c ...
+             * scope idn
+             * scope display_data <color> <invert> <format>
+             */
+
+            Console.WriteLine("Scope!");
+        }
+    }
+}
