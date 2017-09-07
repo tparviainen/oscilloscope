4a930f58ef3605558d88a77ec8202d9cbb4c792c
Tomi Parviainen <tomi.parviainen@gmail.com>
2017-09-07 20:05:19 +0300

    Test RAW commands
    Tests different ways to create a RAW SCPI command from command line style arguments and directly from code i.e. giving extra arguments to Command-method.

---
+++ SCPI.Tests/RAW_Tests.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace SCPI.Tests
+{
+    public class RAW_Tests
+    {
+        [Fact]
+        public void WithoutArguments()
+        {
+            // Arrange
+            var raw = new RAW();
+            var expected = "";
+
+            // Act
+            var actual = raw.Command();
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void EmbeddedArguments()
+        {
+            // Arrange
+            var raw = new RAW();
+            var expected = ":DISPlay:DATA? ON, OFF, TIFF";
+
+            // Act, one command with arguments embedded
+            var actual = raw.Command(":DISPlay:DATA? ON, OFF, TIFF");
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void WithOneExtraArgument()
+        {
+            // Arrange
+            var raw = new RAW();
+            var expected = ":DISPlay:DATA? ON";
+
+            // Act, command line style arguments
+            var actual = raw.Command(":DISPlay:DATA?", "ON");
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void WithSeveralExtraArguments()
+        {
+            // Arrange
+            var raw = new RAW();
+            var expected = ":DISPlay:DATA? ON, OFF, TIFF";
+
+            // Act, command line style arguments
+            var actual = raw.Command(":DISPlay:DATA?", "ON", "OFF", "TIFF");
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void NoExtraArguments()
+        {
+            // Arrange
+            var raw = new RAW();
+            var expected = ":DISPlay:DATA?";
+
+            // Act
+            var actual = raw.Command(":DISPlay:DATA?");
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
