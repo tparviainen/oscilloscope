7107930e5c4c3d2325f5943a52b872d99f197001
Tomi Parviainen <tomi.parviainen@gmail.com>
2017-09-11 19:38:15 +0300

    Add method to query Assembly's containing folder
    Assembly class lacks a method to safely query Assembly's containing folder. This implementation returns the containing folder of the assembly if it can be determined.

---
+++ Scope/AssemblyHelper.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace Scope
+{
+    public static class AssemblyHelper
+    {
+        /// <summary>
+        /// Returns an assembly's containing folder on disk
+        /// </summary>
+        /// <param name="assembly">Assembly whose location will be determined</param>
+        /// <returns>The location of the assembly on disk or null if it
+        /// cannot be determined.</returns>
+        public static string GetAssemblyFolder(Assembly assembly)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(assembly.Location))
+                {
+                    return Path.GetDirectoryName(assembly.Location);
+                }
+
+                if (string.IsNullOrEmpty(assembly.CodeBase))
+                {
+                    return null;
+                }
+
+                var uri = new Uri(assembly.CodeBase);
+                if (!uri.IsFile)
+                {
+                    return null;
+                }
+
+                return Path.GetDirectoryName(uri.LocalPath);
+            }
+            catch (NotSupportedException)
+            {
+                // Dynamic assembly generated with Reflection.Emit
+                return null;
+            }
+        }
+    }
+}
