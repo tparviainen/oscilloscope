803013515e8ff86d616d70a49e0ac21409a1e5d7
Tomi Parviainen <tomi.parviainen@gmail.com>
2017-10-15 20:22:42 +0300

    Implement property changed notification for instrument id

---
+++ Scope.Wpf/Models/Instrument.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scope.Wpf.Models
+{
+    class Instrument : INotifyPropertyChanged
+    {
+        #region Standard pattern for implementing property changed notification
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+
+        private string _Id;
+
+        public string Id
+        {
+            get
+            {
+                return _Id;
+            }
+            set
+            {
+                _Id = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
