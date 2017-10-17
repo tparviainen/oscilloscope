e14c6ea62f7df376e0c92dd9ede5c3a351d42ff0
Tomi Parviainen <tomi.parviainen@gmail.com>
2017-10-17 18:09:00 +0300

    Add WPF UI for scope
    Test version of the WPF UI for scope. Contains basic elements of the UI and the main window in Views-folder.

--- Scope.Wpf/Views/MainWindow.xaml.cs
+++ Scope.Wpf/Views/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Scope.Wpf.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,10 +11,9 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Scope.Wpf
+namespace Scope.Wpf.Views
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -23,6 +23,8 @@ namespace Scope.Wpf
         public MainWindow()
         {
             InitializeComponent();
+
+            DataContext = new MainWindowViewModel();
         }
     }
 }
