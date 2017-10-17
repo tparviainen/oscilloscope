54a32f6f58e04825cbb6f69c6ef459ae3ee68040
Tomi Parviainen <tomi.parviainen@gmail.com>
2017-10-17 18:06:59 +0300

    Add view model for main window
    Draft version of the main window view model, contains functionality to test the connection towards scope using hard coded values.

---
+++ Scope.Wpf/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,78 @@
+﻿using PluginContracts;
+using PluginLoader;
+using Scope.Wpf.Controls;
+using Scope.Wpf.Models;
+using SCPI;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace Scope.Wpf.ViewModels
+{
+    class MainWindowViewModel
+    {
+        public NetworkEndPoint Connection { get; private set; } = new NetworkEndPoint();
+        public Instrument Instrument { get; private set; } = new Instrument();
+        public Screenshot Screenshot { get; private set; } = new Screenshot();
+
+        public Command CmdConnect { get; private set; }
+        public Command CmdFetch { get; private set; }
+
+        private string pluginPath = @".";
+
+        public MainWindowViewModel()
+        {
+            Connection.Port = "5555";
+            Connection.IP = "192.168.1.160";
+
+            CmdConnect = new Command(Connect, null);
+            CmdFetch = new Command(Fetch, () => Instrument.Id != null);
+        }
+
+        private void Connect()
+        {
+            var plugin = Plugins<IPluginV1>.Load(pluginPath).FirstOrDefault();
+
+            plugin.IPEndPoint = Connection.IPEndPoint;
+
+            var commands = new Commands();
+
+            var commandInstance = commands.Get("IDN") as IDN;
+
+            var scpiCommand = commandInstance.Command();
+
+            var data = plugin.SendReceiveAsync(scpiCommand).GetAwaiter().GetResult();
+
+            if (commandInstance.Parse(data))
+            {
+                Instrument.Id = $"{commandInstance.Manufacturer}, {commandInstance.Model}, {commandInstance.SerialNumber}, {commandInstance.SoftwareVersion}";
+                CmdFetch.OnCanExecuteChanged();
+            }
+        }
+
+        private void Fetch()
+        {
+            var plugin = Plugins<IPluginV1>.Load(pluginPath).FirstOrDefault();
+
+            plugin.IPEndPoint = Connection.IPEndPoint;
+
+            var commands = new Commands();
+
+            var commandInstance = commands.Get("DISPLAY_DATA") as DISPLAY_DATA;
+
+            var scpiCommand = commandInstance.Command(/*"ON", "OFF", "PNG"*/);
+
+            var data = plugin.SendReceiveAsync(scpiCommand).GetAwaiter().GetResult();
+
+            if (commandInstance.Parse(data))
+            {
+                Screenshot.Update(commandInstance.ImageData());
+            }
+        }
+    }
+}
