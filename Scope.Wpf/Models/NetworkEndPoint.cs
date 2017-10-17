9cdfc9c1320a6ce8e64a51530b6fbd9771caecad
Tomi Parviainen <tomi.parviainen@gmail.com>
2017-10-17 17:35:37 +0300

    Implement network endpoint class
    Network endpoint class implements INotifyPropertyChanged interface and is able to notify when the value of the port or address changes.

---
+++ Scope.Wpf/Models/NetworkEndPoint.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace Scope.Wpf.Models
+{
+    class NetworkEndPoint : INotifyPropertyChanged
+    {
+        #region Standard pattern for implementing property changed notification
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+
+        private string _Port;
+
+        public string Port
+        {
+            get
+            {
+                return _Port;
+            }
+            set
+            {
+                _Port = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _IP;
+
+        public string IP
+        {
+            get
+            {
+                return _IP;
+            }
+            set
+            {
+                _IP = value;
+                OnPropertyChanged();
+            }
+        }
+
+        /// <summary>
+        /// Network endpoint as an IP address and a port number.
+        /// </summary>
+        public IPEndPoint IPEndPoint
+        {
+            get
+            {
+                return new IPEndPoint(IPAddress.Parse(IP), int.Parse(Port));
+            }
+        }
+    }
+}
