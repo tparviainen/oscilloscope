4b2e3c7d03d4a072414e0320ab76ae237ab75dec
Tomi Parviainen <tomi.parviainen@gmail.com>
2018-06-27 16:16:48 +0300

    Connection interface

---
+++ Scope.Wpf/Models/ConnectionInterface.cs
@@ -0,0 +1,27 @@
+﻿using PluginContracts;
+using Scope.Wpf.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scope.Wpf.Models
+{
+    class ConnectionInterface
+    {
+        public IPluginV1 Plugin { get; private set; }
+        public Command CmdSelect { get; private set; }
+
+        public ConnectionInterface(IPluginV1 plugin)
+        {
+            Plugin = plugin;
+            CmdSelect = new Command(Selected, null);
+        }
+
+        private void Selected()
+        {
+
+        }
+    }
+}
