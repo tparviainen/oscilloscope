ec5a34d841aa7bd8801e0ae999f5db9d123debe3
Tomi Parviainen <tomi.parviainen@gmail.com>
2019-02-03 16:39:48 +0200

    Add unit test for autoscale command

---
+++ SCPI.Tests/AUTOSCALE_Tests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace SCPI.Tests
+{
+    public class AUTOSCALE_Tests
+    {
+        [Fact]
+        public void ValidCommand()
+        {
+            // Arrange
+            var cmd = new AUTOSCALE();
+            var expected = ":AUToscale";
+
+            // Act
+            var actual = cmd.Command();
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
