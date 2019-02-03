77361347482de79a9a65a64bb7ecf1ec86c8556e
Tomi Parviainen <tomi.parviainen@gmail.com>
2019-02-03 16:37:52 +0200

    Add the support for autoscale SCPI command

---
+++ SCPI/AUTOSCALE.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SCPI
+{
+    public class AUTOSCALE : ICommand
+    {
+        public string Description => "The oscilloscope will automatically adjust the vertical scale, horizontal timebase, and trigger mode according to the input signal to realize optimum waveform display";
+
+        public string Command(params string[] parameters) => ":AUToscale";
+
+        public string HelpMessage() => nameof(AUTOSCALE);
+
+        public bool Parse(byte[] data) => true;
+    }
+}
