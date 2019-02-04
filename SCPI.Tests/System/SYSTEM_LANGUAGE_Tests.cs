bc08fbe06e5b1cd33f8fbb2e596ddbf4d5cace06
Tomi Parviainen <tomi.parviainen@gmail.com>
2019-02-04 18:48:58 +0200

    Add unit test for SYSTEM_LANGUAGE SCPI command

---
+++ SCPI.Tests/System/SYSTEM_LANGUAGE_Tests.cs
@@ -0,0 +1,24 @@
+﻿using SCPI.System;
+using System.Text;
+using Xunit;
+
+namespace SCPI.Tests.System
+{
+    public class SYSTEM_LANGUAGE_Tests
+    {
+        [Theory]
+        [InlineData("ENGL")]
+        [InlineData("GERM")]
+        public void LanguageIsSetToProperty(string expected)
+        {
+            // Arrange
+            var cmd = new SYSTEM_LANGUAGE();
+
+            // Act
+            var result = cmd.Parse(Encoding.ASCII.GetBytes($"{expected}\n"));
+
+            // Assert
+            Assert.Equal(expected, cmd.Language);
+        }
+    }
+}
