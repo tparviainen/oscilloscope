235e65167a0a9f7a50235c356bdff04840482f7e
Tomi Parviainen <tomi.parviainen@gmail.com>
2019-02-04 18:28:22 +0200

    Add support for ACQUIRE_SRATE SCPI command

---
+++ SCPI.Tests/Acquire/ACQUIRE_SRATE_Tests.cs
@@ -0,0 +1,52 @@
+﻿using SCPI.Acquire;
+using System.Text;
+using Xunit;
+
+namespace SCPI.Tests.Acquire
+{
+    public class ACQUIRE_SRATE_Tests
+    {
+        [Fact]
+        public void ValidCommand()
+        {
+            // Arrange
+            var cmd = new ACQUIRE_SRATE();
+            var expected = ":ACQuire:SRATe?";
+
+            // Act
+            var actual = cmd.Command();
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void SampleRateIsConvertedSuccessfully()
+        {
+            // Arrange
+            var cmd = new ACQUIRE_SRATE();
+            var data = "1.000000e+09\n";
+            var expected = 1000000000f;
+
+            // Act
+            var result = cmd.Parse(Encoding.ASCII.GetBytes(data));
+
+            // Assert
+            Assert.True(result);
+            Assert.Equal(expected, cmd.SampleRate);
+        }
+
+        [Fact]
+        public void InvalidDataFromInstrument()
+        {
+            // Arrange
+            var cmd = new ACQUIRE_SRATE();
+
+            // Act
+            var result = cmd.Parse(null);
+
+            // Assert
+            Assert.False(result);
+        }
+    }
+}
---
+++ SCPI/Acquire/ACQUIRE_SRATE.cs
@@ -0,0 +1,40 @@
+﻿using System.Globalization;
+using System.Text;
+
+namespace SCPI.Acquire
+{
+    public class ACQUIRE_SRATE : ICommand
+    {
+        public string Description => "Query the current sample rate. The default unit is Sa/s.";
+
+        public float SampleRate { get; private set; }
+
+        public string Command(params string[] parameters) => ":ACQuire:SRATe?";
+
+        public string HelpMessage()
+        {
+            var syntax = nameof(ACQUIRE_SRATE) + "\n";
+            var example = "Example: " + nameof(ACQUIRE_SRATE);
+
+            return $"{syntax}\n{example}";
+        }
+
+        public bool Parse(byte[] data)
+        {
+            if (data == null)
+            {
+                return false;
+            }
+
+            if (float.TryParse(Encoding.ASCII.GetString(data), NumberStyles.Any,
+                               CultureInfo.InvariantCulture, out float sampleRate))
+            {
+                SampleRate = sampleRate;
+
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
