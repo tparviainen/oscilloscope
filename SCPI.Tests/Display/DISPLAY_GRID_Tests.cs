0f0b574520c4e41c3536ddab739267841b853183
Tomi Parviainen <tomi.parviainen@gmail.com>
2019-02-04 21:21:26 +0200

    Add support for DISPLAY_GRID SCPI command

---
+++ SCPI.Tests/Display/DISPLAY_GRID_Tests.cs
@@ -0,0 +1,43 @@
+﻿using SCPI.Display;
+using System.Text;
+using Xunit;
+
+namespace SCPI.Tests.Display
+{
+    public class DISPLAY_GRID_Tests
+    {
+        [Theory]
+        [InlineData("FULL")]
+        [InlineData("HALF")]
+        [InlineData("NONE")]
+        public void CorrectGridValueIsStoredToProperty(string grid)
+        {
+            // Arrange
+            var cmd = new DISPLAY_GRID();
+
+            // Act
+            var result = cmd.Parse(Encoding.ASCII.GetBytes(grid + "\n"));
+
+            // Assert
+            Assert.True(result);
+            Assert.Equal(grid, cmd.Grid);
+        }
+
+        [Theory]
+        [InlineData("MAX")]
+        [InlineData("Half")]
+        [InlineData("Nothing")]
+        public void GridValueIsNotValid(string grid)
+        {
+            // Arrange
+            var cmd = new DISPLAY_GRID();
+
+            // Act
+            var result = cmd.Parse(Encoding.ASCII.GetBytes(grid + "\n"));
+
+            // Assert
+            Assert.False(false);
+            Assert.Null(cmd.Grid);
+        }
+    }
+}
---
+++ SCPI/Display/DISPLAY_GRID.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Text;
+
+namespace SCPI.Display
+{
+    public class DISPLAY_GRID : ICommand
+    {
+        public string Description => "Set or query the grid type of screen display.";
+
+        public string Grid { get; private set; }
+
+        private readonly string[] gridRange = new string[] { "FULL", "HALF", "NONE" };
+
+        public string Command(params string[] parameters)
+        {
+            var cmd = ":DISPlay:GRID";
+
+            if (parameters.Length > 0)
+            {
+                var grid = parameters[0];
+                cmd = $"{cmd} {grid}";
+            }
+            else
+            {
+                cmd += "?";
+            }
+
+            return cmd;
+        }
+
+        public string HelpMessage()
+        {
+            var syntax = nameof(DISPLAY_GRID) + "\n" +
+                         nameof(DISPLAY_GRID) + " <grid>";
+            var parameters = " <grid> = {"+ string.Join("|", gridRange) +"}\n";
+            var example = "Example: " + nameof(DISPLAY_GRID) + "?";
+
+            return $"{syntax}\n{parameters}\n{example}";
+        }
+
+        public bool Parse(byte[] data)
+        {
+            if (data != null)
+            {
+                Grid = Encoding.ASCII.GetString(data).Trim();
+                if (Array.Exists(gridRange, g => g.Equals(Grid)))
+                {
+                    return true;
+                }
+            }
+
+            Grid = null;
+
+            return false;
+        }
+    }
+}
