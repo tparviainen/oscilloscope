5dbd96ab425cc6e07ca8b3d3c3f7f1e7d87376b9
Tomi Parviainen <tomi.parviainen@gmail.com>
2019-02-04 15:42:06 +0200

    Add a support for SYSTEM_LANGUAGE SCPI command

---
+++ SCPI/System/SYSTEM_LANGUAGE.cs
@@ -0,0 +1,38 @@
+﻿namespace SCPI.System
+{
+    public class SYSTEM_LANGUAGE : ICommand
+    {
+        public string Description => "Set or query the system language";
+
+        public string Command(params string[] parameters)
+        {
+            var cmd = ":SYSTem:LANGuage";
+
+            if (parameters.Length > 0)
+            {
+                var language = parameters[0];
+                cmd = $"{cmd} {language}";
+            }
+            else
+            {
+                cmd += "?";
+            }
+
+            return cmd;
+        }
+
+        public string HelpMessage()
+        {
+            var syntax = nameof(SYSTEM_LANGUAGE) + "\n" +
+                         nameof(SYSTEM_LANGUAGE) + " <lang>";
+
+            var parameters = " <lang> = SCH, TCH, ENGL, PORT, GERM, POL, KOR, JAPA, FREN or RUSS\n";
+
+            var example = "Example: " + nameof(SYSTEM_LANGUAGE) + " ENGL";
+
+            return $"{syntax}\n{parameters}\n{example}";
+        }
+
+        public bool Parse(byte[] data) => true;
+    }
+}
