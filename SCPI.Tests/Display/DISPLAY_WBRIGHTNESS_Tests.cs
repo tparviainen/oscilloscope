8fa8940dcc40c88134cf74ff7250c4e271bfb945
Tomi Parviainen <tomi.parviainen@gmail.com>
2019-02-05 18:15:46 +0200

    Implement DISPLAY_WBRIGHTNESS SCPI command + unit tests

---
+++ SCPI.Tests/Display/DISPLAY_WBRIGHTNESS_Tests.cs
@@ -0,0 +1,71 @@
+﻿using SCPI.Display;
+using System;
+using System.Text;
+using Xunit;
+
+namespace SCPI.Tests.Display
+{
+    public class DISPLAY_WBRIGHTNESS_Tests
+    {
+        [Fact]
+        public void ValidQueryCommand()
+        {
+            // Arrange
+            var cmd = new DISPLAY_WBRIGHTNESS();
+            var expected = ":DISPlay:WBRightness?";
+
+            // Act
+            var actual = cmd.Command();
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(50)]
+        [InlineData(100)]
+        public void SetValidBrightnessValue(int brightness)
+        {
+            // Arrange
+            var cmd = new DISPLAY_WBRIGHTNESS();
+            var expected = $":DISPlay:WBRightness {brightness}";
+
+            // Act
+            var actual = cmd.Command(brightness.ToString());
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(101)]
+        public void SetInvalidBrightnessValue(int brightness)
+        {
+            // Arrange
+            var cmd = new DISPLAY_WBRIGHTNESS();
+
+            // Act / Assert
+            Assert.Throws<ArgumentException>(() => cmd.Command(brightness.ToString()));
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(50)]
+        [InlineData(100)]
+        public void ResponseIsParsedSuccessfully(int brightness)
+        {
+            // Arrange
+            var cmd = new DISPLAY_WBRIGHTNESS();
+            var data = $"{brightness}\n";
+
+            // Act
+            var result = cmd.Parse(Encoding.ASCII.GetBytes(data));
+
+            // Assert
+            Assert.True(result);
+            Assert.Equal(brightness, cmd.Brightness);
+        }
+    }
+}
---
+++ SCPI/Display/DISPLAY_WBRIGHTNESS.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using SCPI.Extensions;
+
+namespace SCPI.Display
+{
+    public class DISPLAY_WBRIGHTNESS : ICommand
+    {
+        public string Description => "Set or query the waveform brightness.";
+
+        public int Brightness { get; private set; }
+
+        public string Command(params string[] parameters)
+        {
+            var cmd = ":DISPlay:WBRightness";
+
+            if (parameters.Length > 0)
+            {
+                var brightness = int.Parse(parameters[0]);
+                if (brightness.IsWithin(0, 100))
+                {
+                    cmd = $"{cmd} {brightness}";
+                }
+                else
+                {
+                    throw new ArgumentException();
+                }
+            }
+            else
+            {
+                cmd += "?";
+            }
+
+            return cmd;
+        }
+
+        public string HelpMessage()
+        {
+            var syntax = nameof(DISPLAY_WBRIGHTNESS) + "\n" +
+                         nameof(DISPLAY_WBRIGHTNESS) + " <brightness>";
+            var parameters = " <brightness> = 0 to 100\n";
+            var example = "Example: " + nameof(DISPLAY_WBRIGHTNESS) + " 60";
+
+            return $"{syntax}\n{parameters}\n{example}";
+        }
+
+        public bool Parse(byte[] data)
+        {
+            if (data != null)
+            {
+                if (int.TryParse(Encoding.ASCII.GetString(data), out int brightness))
+                {
+                    Brightness = brightness;
+
+                    return true;
+                }
+            }
+
+            Brightness = 0;
+
+            return false;
+        }
+    }
+}
