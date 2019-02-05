66fb79bafb75672c28d2c12dc563c9102663b0e0
Tomi Parviainen <tomi.parviainen@gmail.com>
2019-02-05 18:13:11 +0200

    Add method to convert scientific notation string to float

--- SCPI/Extensions/ValueTypeExtensions.cs
+++ SCPI/Extensions/ValueTypeExtensions.cs
@@ -1,4 +1,6 @@
-﻿namespace SCPI.Extensions
+﻿using System.Globalization;
+
+namespace SCPI.Extensions
 {
     public static class ValueTypeExtensions
     {
@@ -19,6 +21,23 @@
             }
 
             return ret;
+        }
+
+        /// <summary>
+        /// Converts the string representation of a number to float
+        /// </summary>
+        /// <param name="value">A string representing a number to convert</param>
+        /// <param name="result">When this method returns, contains the single-precision floating-point
+        /// number equivalent to the numeric value or symbol contained in value, if the conversion succeeded</param>
+        /// <returns>True if value was converted successfully; otherwise, false</returns>
+        public static bool ScientificNotationStringToFloat(this string value, out float result)
+        {
+            if (float.TryParse(value, NumberStyles.Any, CultureInfo.InvariantCulture, out result))
+            {
+                return true;
+            }
+
+            return false;
         }
     }
 }
