b39459fecfd39782ca504e9bd00d4aa4f39fdac2
Tomi Parviainen <tomi.parviainen@gmail.com>
2022-05-30 20:08:45 +0300

    Add Avalonia template app

---
+++ Scope.App/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Scope.App.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
